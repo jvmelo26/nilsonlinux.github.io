39f5650310e7b81da826b3f34e8ad5aa5f221a0b
Nilsonlinux <nilsonlinux@gmail.com>
2020-04-24 12:57:13 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,5 @@
 // scrolljump
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122784671-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-122784671-2');
-</script>
-
-
 $(function() {
   $(window).scroll(function() {
     if($(this).scrollTop() != 0) {
